5b3192b9f5310ce69efb9a8bdc89cd3ea50092ac
Ricardo Leidenz <139507934+RicardoLeidenz@users.noreply.github.com>
2025-09-18 00:54:41 +0000

    Added event relationship with users

--- src/app.py
+++ src/app.py
@@ -9,14 +9,15 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User, Event
-#from models import Person
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,14 +28,19 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
 
 @app.route('/user', methods=['GET'])
 def handle_get_users():
@@ -45,16 +51,34 @@ def handle_get_users():
 
     return jsonify(response_body), 200
 
-@app.route('/event', methods=['GET'])
-def handle_get_events():
-    all_events = Event.query.all()
-    response_body = {
-        "data": [x.serialize() for x in all_events]
-    }
 
-    return jsonify({
-        "data": [x.serialize() for x in all_events]
-    }), 200
+@app.route('/event', methods=['GET', 'POST'])
+def handle_events():
+    if request.method == 'POST':
+        keys_to_check = ["what", "where", "who"]
+        new_event = request.get_json()
+        # Check if all information was provided
+        if all(key in new_event for key in keys_to_check):
+            new_event = Event(what=new_event["what"], where=new_event["where"], who=new_event["who"])
+            db.session.add(new_event)
+            db.session.commit()
+            return "New event added: " + new_event
+    else:
+        all_events = Event.query.all()
+        response_body = {
+            "data": [x.serialize() for x in all_events]
+        }
+        return jsonify(response_body), 200
+
+
+@app.route('/event/<int:event_id>', methods=['DELETE'])
+def delete_event(event_id):
+    event = db.session.get(Event, event_id)
+    db.session.delete(event)
+    db.session.commit()
+
+    return f"Event #{event_id} Deleted", 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,13 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, Integer, DateTime, Column
+from sqlalchemy import String, Boolean, Integer, DateTime, Column, ForeignKey
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
+
 
 class User(db.Model):
     id = Column(Integer, primary_key=True)
     email = Column(String(120), unique=True, nullable=False)
-    password = Column(String(120),nullable=False)
+    password = Column(String(120), nullable=False)
     is_active = Column(Boolean(), nullable=False)
+    events_organized = relationship('Event', backref='organizer', lazy=True)
 
     def serialize(self):
         return {
@@ -17,16 +20,17 @@ class User(db.Model):
             # do not serialize the password, its a security breach
         }
 
+
 class Event(db.Model):
     id = Column(Integer, primary_key=True)
     what = Column(String(15), nullable=False)
-    when = Column(DateTime, nullable=False)
     where = Column(String(30), nullable=False)
+    who = Column(Integer, ForeignKey("user.id"), nullable=False)
 
     def serialize(self):
         return {
             "id": self.id,
             "what": self.what,
-            "when": self.when,
-            "where": self.where
+            "where": self.where,
+            "who": self.who
         }
